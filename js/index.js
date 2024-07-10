ff0ad8d92674e9b7944e1ffcda48b7a75c04c0e6
abdurahmonanvarov <dousolo9@gmail.com>
2024-07-10 16:50:29 +0800

    last

---
+++ js/index.js
@@ -0,0 +1,105 @@
+let res = document.getElementById('rang');
+let button = document.getElementById('button');
+let qora = document.getElementById('sarlovha');
+let max = document.getElementById('about');
+let info = document.getElementById('contact');
+let bog = document.getElementById('salom');
+let qayd = document.getElementById('solo');
+let products = document.getElementById('teg');
+let changeNUm = document.getElementById('lkjm');
+let changeNUm1 = document.getElementById('test');
+let resd = document.getElementById('reads');
+let resd1 = document.getElementById('reads2');
+
+let ress = document.getElementById('nula');
+let ressss1 = document.getElementById('esse');
+let ressss2 = document.getElementById('jsdast');
+let ressss3 = document.getElementById('jsdast1');
+let ressss4 = document.getElementById('jsdast3');
+let ressss5 = document.getElementById('jsdast4');
+let ressss6 = document.getElementById('jsdast5');
+let ressss7 = document.getElementById('jsdast6');
+let ressss8 = document.getElementById('jsdast7');
+
+let ressss9 = document.getElementById('fafd');
+let ressss10 = document.getElementById('grffrh');
+
+let ressss11 = document.getElementById('somthing');
+let ressss12 = document.getElementById('nimadur');
+let ressss13 = document.getElementById('nimadur1');
+let ressss14 = document.getElementById('imgg');
+
+
+
+
+
+
+let res2 = 'light';
+button.addEventListener('click', function(element){
+    element.preventDefault();
+    if (res2 == 'light'){
+        res.style.background = 'black';
+        qora.style.color = '#fff';
+        max.style.color = '#fff';
+        info.style.color = '#fff';
+        bog.style.color = '#fff';
+        qayd.style.color = '#fff';
+        products.style.color = '#fff';
+        changeNUm.style.color = '#fff';
+        changeNUm1.style.color = '#fff';
+        resd1.style.color = '#fff';
+        resd.style.color = '#fff';
+
+        ress.style.color = '#fff';
+        ressss1.style.color = '#fff';
+        ressss2.style.color = '#fff';
+        ressss3.style.color = '#fff';
+        ressss4.style.color = '#fff';
+        ressss5.style.color = '#fff';
+        ressss6.style.color = '#fff';
+        ressss7.style.color = '#fff';
+        ressss8.style.color = '#fff';
+        ressss9.style.color = '#fff';
+        ressss10.style.color = '#fff';
+        ressss11.style.color = '#fff';
+        ressss12.style.color = '#fff';
+        ressss13.style.color = '#fff';
+        ressss14.style.background = '#fff';
+
+
+
+
+        res2 = 'dark'
+    }else {
+        res.style.background = '#fff';
+        qora.style.color = '#2B2B2B';
+        max.style.color = '#2B2B2B';
+        info.style.color = '#2B2B2B';
+        bog.style.color = '#2B2B2B';
+        qayd.style.color = '#2B2B2B';
+        products.style.color = '#2B2B2B';
+        changeNUm.style.color = '#2B2B2B';
+        changeNUm1.style.color = '#2B2B2B';
+        resd1.style.color = '#2B2B2B';
+        resd.style.color = '#2B2B2B';
+
+        ress.style.color = '#2B2B2B';
+        ressss1.style.color = '#2B2B2B';
+        ressss2.style.color = '#2B2B2B';
+        ressss3.style.color = '#2B2B2B';
+        ressss4.style.color = '#2B2B2B';
+        ressss5.style.color = '#2B2B2B';
+        ressss6.style.color = '#2B2B2B';
+        ressss7.style.color = '#2B2B2B';
+        ressss8.style.color = '#2B2B2B';
+        ressss9.style.color = '#2B2B2B';
+        ressss10.style.color = '#2B2B2B';
+        ressss11.style.color = '#2B2B2B';
+        ressss12.style.color = '#2B2B2B';
+        ressss13.style.color = '#2B2B2B';
+
+
+
+        res2 = 'light';
+    }
+})
